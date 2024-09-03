c1ab814bec748554fa849da8ea7b8a5bbe83e6c4
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2024-09-02 17:29:08 -0700

    refactor all routes

--- routes/envelopes.js
+++ routes/envelopes.js
@@ -1,70 +1,122 @@
 const express = require("express")
 const router = express.Router()
 
-const envelopes = require("../config/db")
-const { createId, findById } = require("../utils")
+const dbEnvelopes = require("../config/db")
+const { createId, findById, deleteById } = require("../utils")
 
-router.param("envelopeId", (req, res, next, id) => {
-  const envelope = findById(envelopes, id)
-
-  if (!envelope) {
-    res.status(404).send("Envelope not found")
-  } else {
-    req.envelope = envelope
-    next()
+router.get("/", async (req, res) => {
+  try {
+    const envelopes = await dbEnvelopes
+    res.status(200).send(envelopes)
+  } catch (err) {
+    res.status(400).send(err)
   }
 })
 
-router.get("/", (req, res) => {
-  if (!envelopes || envelopes.length < 1) {
-    res.status(404).send("No envelopes found.")
-  } else {
-    res.send(envelopes)
-  }
-})
-
-router.post("/", (req, res) => {
-  if (!req.body?.title || !req.body?.budget) {
-    res.status(400).send("A valid title and budget are required")
-    return
-  }
-
-  const { title, budget } = req.body
-  const newId = createId(envelopes)
-  const newEnvelope = {
-    id: newId,
-    title,
-    budget,
-  }
-
-  envelopes.push(newEnvelope)
-
-  res.status(201).send(newEnvelope)
-})
-
-router.get("/:envelopeId", (req, res) => {
-  res.send(req.envelope)
-})
-
-router.post("/:envelopeId", (req, res) => {
-  if (req.body?.expense && typeof req.body?.expense === "number") {
-    const expense = req.body.expense
-    const newBudget = req.envelope.budget - expense
-
-    if (newBudget < 0) {
-      res.send("Expense exceeds budget")
-    } else {
-      req.envelope.budget = newBudget
-      res.send(req.envelope)
+router.post("/", async (req, res) => {
+  try {
+    const { title, budget } = req.body
+    const envelopes = await dbEnvelopes
+    const newId = createId(envelopes)
+    const newEnvelope = {
+      id: newId,
+      title,
+      budget,
     }
-  } else {
-    res.status(400).send("Must include a valid expense")
+
+    envelopes.push(newEnvelope)
+    res.status(201).send(newEnvelope)
+  } catch (err) {
+    res.status(500).send(err)
   }
 })
 
-router.delete("/:envelopeId", (req, res) => {
-  envelopes.splice([envelopes.indexOf(req.envelope)], 1)
-  res.send("Envelope deleted")
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const envelopes = await dbEnvelopes
+    const envelope = findById(envelopes, id)
+
+    if (!envelope) {
+      return res.status(404).send({
+        message: "Envelope Not Found",
+      })
+    }
+
+    res.status(200).send(envelope)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { title, budget } = req.body
+    const { id } = req.params
+    const envelopes = await dbEnvelopes
+    const envelope = findById(envelopes, id)
+
+    if (!envelope) {
+      return res.status(404).send({
+        message: "Envelope Not Found",
+      })
+    }
+
+    envelope.title = title
+    envelope.budget = budget
+    res.status(201).send(envelopes)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const envelopes = await dbEnvelopes
+    const envelope = findById(envelopes, id)
+
+    if (!envelope) {
+      return res.status(404).send({
+        message: "Envelope Not Found",
+      })
+    }
+
+    const updatedEnvelopes = deleteById(envelopes, id)
+    res.status(204).send(updatedEnvelopes)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.post("/:fromId/transfer/:toId", async (req, res) => {
+  try {
+    const envelopes = await dbEnvelopes
+    const { fromId, toId } = req.params
+    const { amount } = req.body
+
+    const originEnv = findById(envelopes, fromId)
+    const destinationEnv = findById(envelopes, toId)
+
+    if (!originEnv || !destinationEnv) {
+      return res.status(404).send({
+        message: "Envelope Not Found",
+      })
+    }
+
+    if (originEnv.budget < amount) {
+      return res.status(400).send({
+        message: "Amount to transfer exceeds envelope budget funds",
+      })
+    }
+
+    originEnv.budget -= amount
+    destinationEnv.budget += amount
+
+    res.status(201).send(originEnv)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 })
 
 module.exports = router
